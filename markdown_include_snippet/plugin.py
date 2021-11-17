77cf2cb7998152e9f97ab453980a9b776014899f
JarvisZeng <jarviszeng.zjc@qq.com>
2021-11-17 18:26:47 +0800

    support get markdown source from local

--- markdown_include_snippet/plugin.py
+++ markdown_include_snippet/plugin.py
@@ -16,10 +16,23 @@ class MarkdownIncludeSnippetPlugin(BasePlugin):
     config_scheme = (
         ("base_path", mkdocs.config.config_options.Type(str, default="docs")),
         ("all_pages", mkdocs.config.config_options.Type(bool, default=True)),
+        ("encoding", mkdocs.config.config_options.Type(str, default="utf-8")),
     )
     page = None
 
-    def _markdown_snippet(self, repository: str, file: str, ref: str, section: str) -> str:
+    def _resource_from_local(self, file: str, section: str) -> str:
+        if not os.path.isabs(file):
+            file = os.path.normpath(os.path.join(os.getcwd(), self.config["base_path"], file))
+        try:
+            with open(file, "r", encoding=self.config["encoding"]) as fr:
+                content = fr.read()
+                if section:
+                    content = get_markdown_section(content, section)
+                return content
+        except Exception as e:
+            raise Exception(f"No such file: {file}")
+
+    def _markdown_snippet(self, file: str, repository: str, ref: str, section: str) -> str:
         g = Github(os.getenv("GITHUB_TOKEN"))
         repo = g.get_repo(repository)
         f = repo.get_contents(file, ref=ref)
@@ -32,6 +45,20 @@ class MarkdownIncludeSnippetPlugin(BasePlugin):
         return content
 
     def snippet(
+        self,
+        file: str,
+        section: str = None,
+        repository: str = None,
+        ref: str = GithubObject.NotSet,
+    ) -> str:
+        if repository:
+            print("use git")
+            return self._markdown_snippet(file, repository, ref, section)
+        else:
+            print("use local")
+            return self._resource_from_local(file, section)
+
+    def snippet_old(
         self,
         repository: str,
         file: str,
